8c82be9f6182793659d084444e2fecc1500be96b
sashablokhin <sashablokhindev@gmail.com>
2016-02-17 12:09:20 +0300

    Creating a request router

--- AlamofirePhotos/Five100px.swift
+++ AlamofirePhotos/Five100px.swift
@@ -7,9 +7,42 @@
 //
 
 import Foundation
+import Alamofire
 
 
 struct Five100px {
+    
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.500px.com/v1"
+        static let consumerKey = "uiGZOMxiSv7SJITSMZ2G4nwZwLe8Ek0j9SaE4nr0"
+        
+        case PopularPhotos(Int)
+        case PhotoInfo(Int, ImageSize)
+        case Comments(Int, Int)
+        
+        var URLRequest: NSMutableURLRequest {
+            let request: (path: String, parameters: [String : AnyObject]) = {
+                switch self {
+                case .PopularPhotos (let page):
+                    let params = ["consumer_key": Router.consumerKey, "page": "\(page)", "feature": "popular", "rpp": "50",  "include_store": "store_download", "include_states": "votes"]
+                    return ("/photos", params)
+                case .PhotoInfo(let photoID, let imageSize):
+                    let params = ["consumer_key": Router.consumerKey, "image_size": "\(imageSize.rawValue)"]
+                    return ("/photos/\(photoID)", params)
+                case .Comments(let photoID, let commentsPage):
+                    let params = ["consumer_key": Router.consumerKey, "comments": "1", "comments_page": "\(commentsPage)"]
+                    return ("/photos/\(photoID)/comments", params)
+                }
+            } ()
+            
+            let URL = NSURL(string: Router.baseURLString)
+            let URLRequest = NSURLRequest(URL: URL!.URLByAppendingPathComponent(request.path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: request.parameters).0
+        }
+    }
+    
     enum ImageSize: Int {
         case Tiny = 1
         case Small = 2
